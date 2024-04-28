34c1e5dcaf43a8ecf6c87025a1285119902129c9
raunak <r.singh2005@outlook.com>
2024-04-28 21:39:57 +0100

    Comments

--- Client.py
+++ Client.py
@@ -14,11 +14,14 @@ import sys
 # Function to get the network information from the user
 def getNetworkInfo():
 
+    # Requesting server IP and port from the user
     HOST = simpledialog.askstring('IP Address', 'Please Enter the Server IP Address: ')
     PORT = simpledialog.askstring('Port', 'Please Enter the Server Port: ')
+    # Validating the IP address format
     if not re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', HOST):
         print("Invalid IP address format. Please try again.")
         return getNetworkInfo()
+    # Validating the port number
     if not PORT.isdigit() or not 0 <= int(PORT) <= 65535:
         print("Invalid port number. Please try again.")
         return getNetworkInfo()
@@ -34,6 +37,7 @@ class Client:
 
     # Constructor
     def __init__(self, host, port):
+        # Create the client socket
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((host, port))
@@ -44,12 +48,14 @@ class Client:
         msg = tkinter.Tk()
         msg.withdraw()
 
+        # Requesting the nickname from the user
         try:
             self.nickname = simpledialog.askstring('Nickname', 'Please choose a nickname', parent=msg)
         except Exception as e:
             print(f'Error in getting the nickname: {e}')
             exit(0)
 
+        # Requesting the password from the user
         if self.nickname == 'ADMIN':
             try:
                 self.plaintextPassword = simpledialog.askstring('Password', 'Please enter the admin password', parent=msg)
@@ -58,12 +64,14 @@ class Client:
                 print(f'Error in getting the password: {e}')
                 exit(0)
 
+        # Create the public and private keys and assign flags
         self.stopFlag = threading.Event()
         self.guiDone = False
         self.running = True
         self.pubkey, self.privkey = rsa.newkeys(1024)
         self.serverPubkey = None
 
+        # Exchange the public keys
         try:
             self.serverPubkey = rsa.PublicKey.load_pkcs1(self.sock.recv(1024))
             self.sock.send(self.pubkey.save_pkcs1("PEM"))
@@ -71,6 +79,7 @@ class Client:
             print(f'Error in exchanging the public keys: {e}')
             exit(0)
 
+        # Start the GUI and receive threads
         guiThread = threading.Thread(target=self.guiLoop)
         receiveThread = threading.Thread(target=self.receive)
 
@@ -79,25 +88,33 @@ class Client:
 
     #  Function to create the GUI
     def guiLoop(self):
+
+        # Create the GUI
         try:
+            # Create the GUI window
             self.win = tkinter.Tk()
             self.win.configure(bg='lightgray')
 
+            # Create the chat label
             self.chatLabel = tkinter.Label(self.win, text='Chat:', bg='lightgray')
             self.chatLabel.config(font=('Arial', 12))
             self.chatLabel.pack(padx=20, pady=5)
 
+            # Create the text area
             self.textArea = tkinter.scrolledtext.ScrolledText(self.win)
             self.textArea.pack(padx=20, pady=5)
             self.textArea.config(state='disabled')
 
+            # Create the message label
             self.msgLabel = tkinter.Label(self.win, text='Message:', bg='lightgray')
             self.msgLabel.config(font=('Arial', 12))
             self.msgLabel.pack(padx=20, pady=5)
 
+            # Create the input area
             self.inputArea = tkinter.Text(self.win, height=3)
             self.inputArea.pack(padx=20, pady=5)
 
+            # Create the send and leave buttons
             self.sendButton = tkinter.Button(self.win, text='send', command=self.write)
             self.sendButton.config(font=('Arial', 12))
             self.sendButton.pack(padx=20, pady=5)
@@ -112,6 +129,7 @@ class Client:
 
             self.win.mainloop()
 
+            # Update the GUI
             while self.win:
                 self.win.update()
                 if self.stopFlag.is_set():
@@ -125,27 +143,39 @@ class Client:
     # Function to send the message to the server
     def write(self):
         try:
+            # Get the message from the input area
             msg = f'{self.nickname}: {self.inputArea.get("0.1", "end")}'
             message = msg.rstrip('\n')
+
+            # Check if the message is a command
             if message[len(self.nickname) + 2:].startswith('/'):
+                # Check if the client is the admin
                 if self.nickname == 'ADMIN':
+                    # Check if the command is to kick a user
                     if message[len(self.nickname) + 2:].startswith('/kick'):
+                        # Send the kick command to the server
                         self.sock.send(
                             rsa.encrypt(f'KICK {message[len(self.nickname) + 8:]}'.encode('utf-8'), self.serverPubkey))
                         self.inputArea.delete('0.1', 'end')
-
+                    # Check if the command is to ban a user
                     elif message[len(self.nickname) + 2:].startswith('/ban'):
+                        # Send the ban command to the server
                         self.sock.send(
                             rsa.encrypt(f'BAN {message[len(self.nickname) + 7:]}'.encode('utf-8'), self.serverPubkey))
                         self.inputArea.delete('0.1', 'end')
+                    # Send the message to the server
                     else:
                         self.sock.send(rsa.encrypt(message.encode('utf-8'), self.serverPubkey))
                         self.inputArea.delete('0.1', 'end')
+
+                # Refuse the command if the client is not the admin
                 else:
                     self.textArea.config(state='normal')
                     self.textArea.insert('end', f'you are not authorized to use this command\n')
                     self.textArea.yview('end')
                     self.textArea.config(state='disabled')
+
+            # Send the message to the server
             else:
                 self.sock.send(rsa.encrypt(message.encode('utf-8'), self.serverPubkey))
                 self.inputArea.delete('0.1', 'end')
@@ -175,20 +205,28 @@ class Client:
     def receive(self):
         while self.running:
             try:
+                # Receive and decrypt the message
                 message = rsa.decrypt(self.sock.recv(1024), self.privkey).decode('utf-8')
+
+                # Check if the message is a command
                 if message == 'NICK':
+                    # Send the nickname to the server
                     self.sock.send(rsa.encrypt(self.nickname.encode('utf-8'), self.serverPubkey))
                     nextMessage = rsa.decrypt(self.sock.recv(1024), self.privkey).decode('utf-8')
                     if nextMessage == "PASS":
+                        # Send the password to the server
                         self.sock.send(rsa.encrypt(self.hashPassword.encode('utf-8'), self.serverPubkey))
-                        nextMessage = rsa.decrypt(self.sock.recv(1024), self.privkey).decode('utf-8')  # Add this line
-                        if nextMessage == 'REFUSE':  # Check if the next message is 'REFUSE'
+                        nextMessage = rsa.decrypt(self.sock.recv(1024), self.privkey).decode('utf-8')
+                        # Check if the password is correct
+                        if nextMessage == 'REFUSE':
                             print('Connection refused!!')
                             self.stop()
+                    # Check if the client is banned
                     elif nextMessage == 'BANNED':
                         print('connection refused because of ban')
                         self.stop()
                 else:
+                    # Display the message in the GUI
                     if self.guiDone:
                         self.textArea.config(state='normal')
                         self.textArea.insert('end', f'{message}\n')
@@ -201,7 +239,7 @@ class Client:
                 self.sock.close()
                 break
 
-
+# Function to handle the interrupt signal
 def signal_handler(sig, frame):
     print("Shutting down client...")
     client.stop()
--- Server.py
+++ Server.py
@@ -11,11 +11,14 @@ import signal
 # Function to get the network information from the user
 def getNetworkInfo():
 
+    # Requesting server IP and port from the user
     HOST = input('Please Enter the Server IP Address: ')
     PORT = input('Please Enter the Server Port: ')
+    # Validating the IP address format
     if not re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', HOST):
         print("Invalid IP address format. Please try again.")
         return getNetworkInfo()
+    # Validating the port number
     if not PORT.isdigit() or not 0 <= int(PORT) <= 65535:
         print("Invalid port number. Please try again.")
         return getNetworkInfo()
@@ -39,13 +42,18 @@ clients_data = []
 
 # Function to hash the admin password
 def hashAdminPassword():
+
+    # Requesting password from the user
     plainPassword = input('Please Enter a Password: ')
+    # Hashing the password
     hashPassword = hashlib.sha256(plainPassword.encode('utf-8')).hexdigest()
     return hashPassword
 
 
 # Function to broadcast the message to all the clients
 def broadcast(msg):
+
+    # Looping through all clients and sending the message
     for data_row in clients_data:
         try:
             data_row[0].send(rsa.encrypt(msg.encode('utf-8'), data_row[2]))
@@ -55,43 +63,61 @@ def broadcast(msg):
 
 # Function to handle the incoming connections
 def receive():
+
+    # Hashing the admin password
     realPassword = hashAdminPassword()
     while True:
         try:
+            # Accepting a new client connection
             client, addr = server.accept()
             print(f'connected with {str(addr)}!')
 
+            # Sending the public key to the client
             client.send(pubkey.save_pkcs1("PEM"))
+            # Receiving the client's public key
             client_public_key = rsa.PublicKey.load_pkcs1(client.recv(1024))
 
+            # Requesting the client's nickname
             client.send(rsa.encrypt('NICK'.encode('utf-8'), client_public_key))
 
             try:
+                # Receiving the client's nickname
                 nickname = rsa.decrypt(client.recv(1024), privkey).decode('utf-8')
             except:
                 continue
 
+            # Checking if the client is banned
             with open('bans.txt', 'r') as f:
                 bans = f.readlines()
 
             if nickname + '\n' in bans:
+                # Informing the client that they are banned
                 client.send(rsa.encrypt('BANNED'.encode('utf-8'), client_public_key))
 
+            # Checking if the client is the admin
             if nickname == "ADMIN":
+                # Requesting the admin password
                 client.send(rsa.encrypt('PASS'.encode('utf-8'), client_public_key))
+                # Receiving the hashed password
                 hashPassword = rsa.decrypt(client.recv(1024), privkey).decode('utf-8')
 
+                # Checking if the password is correct
                 if hashPassword != realPassword:
+                    # Refusing the connection if the password is incorrect
                     client.send(rsa.encrypt('REFUSE'.encode('utf-8'), client_public_key))
                     client.close()
                     continue
 
+            # Adding the client to the clients list
             clients_data.append([client, nickname, client_public_key])
 
             print(f'Nickname of the client is {nickname}')
+            # Broadcasting that the client has connected
             broadcast(f'{nickname} has connected to the server \n')
+            # Informing the client that they have connected
             client.send(rsa.encrypt("Connected to the server\n".encode('utf-8'), client_public_key))
 
+            # Starting a new thread to handle the client's messages
             thread = threading.Thread(target=handle, args=(client,))
             thread.start()
         except rsa.DecryptionError:
@@ -104,62 +130,84 @@ def receive():
 
 # Function to handle the interrupt signal
 def signal_handler(sig, frame):
+
     print("Shutting down server...")
+    # Closing the server socket
     server.close()
     sys.exit(0)
 
 
 # Function to handle the client messages
 def handle(client):
+
     while True:
         try:
+            # Receiving and decrypting the client's message
             msg = decMessage = rsa.decrypt(client.recv(1024), privkey).decode("utf-8")
             print(decMessage)
 
+            # Checking if the message is a command
             if msg.startswith('KICK') or msg.startswith('BAN'):
+                # Checking if the client is the admin
                 clientDataRow = next(row for row in clients_data if row[0] == client)
                 if clients_data[clients_data.index(clientDataRow)][1] != 'ADMIN':
+                    # Refusing the command if the client is not the admin
                     client.send(rsa.encrypt('Command Refused'.encode('utf,8'), clientDataRow[2]))
                     continue
 
+                # Checking if the command is to kick a user
                 if msg.startswith('KICK'):
                     nameToKick = msg[5:]
                     kickUser(nameToKick)
 
+                # Checking if the command is to ban a user
                 elif msg.startswith('BAN'):
                     nameToBan = msg[4:]
                     kickUser(nameToBan)
+                    # Adding the user to the ban list
                     with open('bans.txt', 'a') as f:
                         f.write(f'{nameToBan}\n')
                     print(f'{nameToBan} was banned')
 
+            # Checking if the message is to leave the chat
             elif msg.startswith('LEAVE'):
                 nameToLeave = msg[6:]
                 clientDataRow = next((row for row in clients_data if row[1] == nameToLeave), None)
                 if clientDataRow:
+                    # Removing the client from the clients list
                     clients_data.remove(clientDataRow)
+                    # Closing the client's connection
                     clientDataRow[0].shutdown(socket.SHUT_RDWR)
                     clientDataRow[0].close()
+                    # Broadcasting that the client has left
                     broadcast(f'{nameToLeave} has left the chat!')
 
             else:
+                # Broadcasting the client's message
                 broadcast(decMessage)
 
         except:
+            # Removing the client from the clients list and closing their connection if an error occurs
             clientDataRow = next((row for row in clients_data if row[0] == client), None)
             if clientDataRow:
                 clients_data.remove(clientDataRow)
                 client.close()
+                # Broadcasting that the client has disconnected
                 broadcast(f'{clientDataRow[1]} disconnected from the server!')
             break
 
 
 # Function to kick the user
 def kickUser(name):
+
+    # Finding the user in the clients list
     client_data_row = next((row for row in clients_data if row[1] == name), None)
     if client_data_row:
+        # Broadcasting that the user was kicked
         broadcast(f'{name} was kicked!')
+        # Removing the user from the clients list
         clients_data.remove(client_data_row)
+        # Closing the user's connection
         client_data_row[0].shutdown(socket.SHUT_RDWR)
         client_data_row[0].close()
 
